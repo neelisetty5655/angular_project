5111bbf5e4179dfa04a5bd56f43c744d9b2ee9d9
tkasvala9 <twinkalkasvala@gmail.com>
2022-03-03 15:44:56 +0530

    created-componant

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CustomerComponent } from './customer/customer.component';
+import { BuyerComponent } from './buyer/buyer.component';
+import { SellerComponent } from './seller/seller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CustomerComponent,
+    BuyerComponent,
+    SellerComponent
   ],
   imports: [
     BrowserModule,
